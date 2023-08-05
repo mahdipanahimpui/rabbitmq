fd73e195bdaa6d938508b28759ce78f925cf80a3
mahdi <mahdipanahimpui@gmail.com>
2023-08-05 14:02:55 +0330

    exchange_to_exchange

--- exchange_to_exchange/client.py
+++ exchange_to_exchange/client.py
@@ -8,6 +8,18 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',
 channel = connection.channel()
 
 
+channel.exchange_declare(exchange='sourceEx', exchange_type='direct')
+channel.exchange_declare(exchange='destinationEx', exchange_type='fanout')
+
+channel.exchange_bind(destination='destinationEx', source='sourceEx')
+ 
+# in dirct, routing_key is required 
+channel.basic_publish(exchange='sourceEx', routing_key='', body='message from publisher.')
+
+print('message Sent ...')
+
+connection.close()
+
 
 
 
--- exchange_to_exchange/server.py
+++ exchange_to_exchange/server.py
@@ -8,10 +8,18 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',
 
 channel = connection.channel()
 
+channel.exchange_declare(exchange='destinationEx', exchange_type='fanout')
 
+channel.queue_declare(queue='my_queue')
+
+channel.queue_bind(exchange='destinationEx', queue='my_queue')
     
 
-channel.basic_consume(queue='request_queue', auto_ack=True, on_message_callback=on_request_message_received)
+def callback(channel, mehtod, properties, body):
+    print(f'Received: {body}')
+
+
+channel.basic_consume(queue='my_queue', auto_ack=True, on_message_callback=callback)
 
 
 print('server is running ...')
